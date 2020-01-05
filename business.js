2fa11759aa94c1bf2ae7f69aaa9232aa67d3aebe
Vetalmoff <Vetalmoff@gmail.com>
2020-01-05 18:03:27 +0200

    right answer

--- business.js
+++ business.js
@@ -3,7 +3,7 @@
 let money = prompt("Ваш Бюджет на місяць", "1000"); 
 let time = prompt("Input Date YYYY-MM-DD", "2020-01-01");
 let appData = {
-    money,
+    budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
@@ -13,26 +13,23 @@ let appData = {
 
 let first = prompt("Введіть обовязкову статтю видатків в цьому місяці", "Car");
 let second = prompt("Скільки це нам коштуватиме?", "100");
+let third = prompt("Введіть обовязкову статтю видатків в цьому місяці", "Bike");
+let fourth= prompt("Скільки це нам коштуватиме?", "200");
 
 /*  let expenses = {
      [first]: second,
 };  */
 
- let expenses = {};
- expenses.first = second;
+ appData.expenses.first = second;
+ appData.expenses.third = fourth;
 
-alert( expenses.first );
-
-
-console.log(expenses);
-
-alert(+money / 30);
+alert(appData.budget / 30);
 
 
 console.log(money);
 console.log(time);
 console.log(first);
 console.log(second);
-console.log(appData);
+
 
 
